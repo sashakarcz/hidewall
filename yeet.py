89ad177835a7c25b3e863294d54c6e55d77742d0
lathama <lathama@gmail.com>
2023-08-13 13:02:44 -0600

    lathama being lathama

--- yeet.py
+++ yeet.py
@@ -1,83 +1,56 @@
-import bjoern
-import requests
-import logging
-from bs4 import BeautifulSoup
-from urllib.parse import quote_plus
-from flask import Flask, request, render_template_string, send_from_directory
-
-logging.basicConfig(level=logging.INFO)
-app = Flask(__name__, static_url_path='/static')
-
-# HTML template for the homepage
-home_template = """
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Hidewalls</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<link rel="stylesheet" href="static/fonts/material-design-iconic-font/css/material-design-iconic-font.min.css">
-
-<link rel="stylesheet" href="static/css/style.css">
-<meta name="robots" content="noindex, follow">
-<link rel="manifest" href="{{ url_for('static', filename='manifest.json') }}">
-<script>
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js', { scope: '/' })
-      .then(registration => {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch(error => {
-        console.log('Service Worker registration failed:', error);
-      });
-  }
-</script>
-</head>
-<body>
-  <div class="wrapper">
-    <div class="inner">
-      <form method="GET" action="/yeet">
-      <h3>Be Gone Paywalls!</h3>
-      <p>Paste the URL to the site you wish to visit in the box, and then click Remove Paywalls.</p>
-      <label class="form-group">
-        <input type="text" name="y" class="form-control" required>
-        <span>http://news.site/blocked</span>
-        <span class="border"></span>
-      </label>
-      <button>Remove Paywalls</button>
-     </form>
-    </div>
-  </div>
-      {% if results %}
-    <ul>
-        {% for result in results %}
-        <li><a href="{{ result['link'] }}">{{ result['title'] }}</a></li>
-        {% endfor %}
-    </ul>
-    {% endif %}
- </body>
-</html>
+"""
+Hidewall is a Python Flask app that uses the Google Web Cache, or
+Wayback Machine to access content that is blocked behind a soft paywall.
 """
 
-@app.route("/")
+import logging
+from urllib.parse import quote_plus
+import requests
+import bjoern
+from bs4 import BeautifulSoup
+from flask import Flask, request, render_template, send_from_directory
+
+HOST = '0.0.0.0'
+PORT = 80
+TEMPLATE = 'index.html'
+JAVASCRIPT = 'service-worker.js'
+STATICURLPATH = '/static'
+CACHE_GOOGLE = 'http://webcache.googleusercontent.com/search?q=cache:'
+CACHE_ARCHIVEORG = 'https://web.archive.org/web/'
+APPROUTE_ROOT = '/'
+APPROUTE_JS = '/' + JAVASCRIPT
+APPROUTE_APP = '/yeet'
+
+logging.basicConfig(level=logging.INFO)
+app = Flask(__name__, static_url_path=STATICURLPATH)
+
+@app.route(APPROUTE_ROOT)
 def index():
-    return render_template_string(home_template)
+    """
+    Display the homepage from template
+    """
+    return render_template(TEMPLATE)
 
-@app.route("/service-worker.js")
+@app.route(APPROUTE_JS)
 def service_worker():
-    return send_from_directory('.', 'service-worker.js')
+    """
+    Present the JS for browser
+    """
+    return send_from_directory('.', JAVASCRIPT)
 
-@app.route("/yeet")
+@app.route(APPROUTE_APP)
 def search():
+    """
+    Lookup a source from various caches
+    """
     query = request.args.get("y", "")
-    url_query = request.args.get("url_query", "")
+    # url_query = request.args.get("url_query", "")
 
     if query:
         try:
-            base_url = "http://webcache.googleusercontent.com/search?q=cache:"
+            base_url = CACHE_GOOGLE
             if "nytimes.com" in query:
-                base_url = "https://web.archive.org/web/"
+                base_url = CACHE_ARCHIVEORG
 
             # Generate the complete query URL
             query_url = f"{base_url}{quote_plus(query)}"
@@ -98,17 +71,13 @@ def search():
             # Return the parsed content as a response
             return rendered_content
 
-        except Exception as e:
-            # Handle exceptions and return an error response
-            error_message = f"An error occurred: {str(e)}"
+        except Exception as an_err:
+            error_message = f"Unexpected {an_err=}, {type(an_err)=}"
             return error_message, 500
 
     # Handle the case where query is empty
     return "No query provided", 400
 
 if __name__ == "__main__":
-    host = '0.0.0.0'
-    port = 80
-    print(f"Starting server on {host}:{port}")
-    bjoern.run(app, host, port)
-
+    print(f"Starting server on {HOST}:{PORT}")
+    bjoern.run(app, HOST, PORT)
