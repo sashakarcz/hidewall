75f8151bed8936d2c99ec4ffa56b714da0220d2d
sasha <sasha@starnix.net>
2023-10-13 21:54:42 -0500

    Adding back in parsing web caches in addition to using requests, depending on URL

--- yeet.py
+++ yeet.py
@@ -6,6 +6,7 @@ to access content that is blocked behind a soft paywall.
 import logging
 import os
 import re
+from urllib.parse import quote_plus  # Added import
 import requests
 import bjoern
 from bs4 import BeautifulSoup
@@ -16,10 +17,17 @@ PORT = int(os.environ.get("PORT", 80))
 HOST = '0.0.0.0'
 TEMPLATE = 'index.html'
 JAVASCRIPT = 'service-worker.js'
+CACHE_GOOGLE = 'http://webcache.googleusercontent.com/search?q=cache:'
+CACHE_ARCHIVEORG = 'https://web.archive.org/web/'
+CACHE_ARCHIVE = 'https://archive.is/latest/'
 STATICURLPATH = '/static'
 APPROUTE_ROOT = '/'
 APPROUTE_JS = '/' + JAVASCRIPT
 APPROUTE_APP = '/yeet'
+
+# Read the list of blocked sites from the file
+with open('blocked_sites.txt', 'r') as file:
+    blocked_sites = [line.strip() for line in file]
 
 logging.basicConfig(level=logging.INFO)
 app = Flask(__name__, static_url_path=STATICURLPATH)
@@ -41,7 +49,7 @@ def service_worker():
 @app.route(APPROUTE_APP)
 def search():
     """
-    Download URL via requests and re-serve it using BeautifulSoup
+    Checks the URL, then decides to use a web cache or requests to render content
     """
 
     query = request.args.get("y", "")
@@ -51,33 +59,20 @@ def search():
             # Validate the input URL
             if not is_valid_url(query):
                 return "Invalid URL provided", 400
-            # Retrieve User-Agent header from the request
-            user_agent = request.headers.get("User-Agent")
 
-            # Define headers dictionary with User-Agent
-            headers = {'User-Agent': user_agent}
+            rendered_content = request_url(query)  # Capture the result
+            if any(site in query for site in blocked_sites):
+                rendered_content = use_cache(query)  # Capture the result
 
-            response = requests.get(query, headers=headers)
+            return rendered_content  # Return the result
 
-            # Parse the entire page content using BeautifulSoup
-            soup = BeautifulSoup(response.text, "html.parser")
-
-            # Render the parsed content as a string
-            rendered_content = soup.prettify()
-
-            # Return the parsed content as a response
-            return rendered_content
-
-        except Exception as an_err:
+        except requests.exceptions.RequestException as an_err:
             # Log the error for debugging purposes
-            #error_message = f"Unexpected {an_err=}, {type(an_err)=}"
-            #return error_message, 500
             logging.error("An error occurred: %s", str(an_err))
             return "An error occurred", 500
 
     # Handle the case where query is empty
     return "No query provided", 400
-
 def is_valid_url(url):
     """
     Validate if a given URL is valid.
@@ -85,6 +80,61 @@ def is_valid_url(url):
     # Use a regex pattern for basic URL validation
     pattern = re.compile(r'^(https?://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|])')
     return bool(pattern.match(url))
+
+def request_url(url):
+    """
+    Download URL via requests and servce it using BeautifulSoup
+    """
+
+    # Retrieve User-Agent header from the request
+    user_agent = request.headers.get("User-Agent")
+
+    # Define headers dictionary with User-Agent
+    headers = {'User-Agent': user_agent}
+
+    response = requests.get(url, headers=headers, timeout=10)
+
+    # Parse the entire page content using BeautifulSoup
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Render the parsed content as a string
+    rendered_content = soup.prettify()
+
+    # Return the parsed content as a response
+    return rendered_content
+
+def use_cache(url):
+    """
+    Uses a web cache to download site, then remove any headers that have been added.
+    """
+    # Generate the complete query URL
+    base_url = CACHE_GOOGLE
+    query_url = f"{base_url}{quote_plus(url)}"
+
+    # Retrieve User-Agent header from the request
+    user_agent = request.headers.get("User-Agent")
+
+    # Define headers dictionary with User-Agent
+    headers = {
+        "User-Agent": user_agent
+    }
+
+    response = requests.get(query_url, headers=headers, timeout=10)
+
+    # Parse the entire page content using BeautifulSoup
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Remove header elements
+    selectors_to_remove = '[id*="google-cache-hdr"], [id*="wm-ipp"], [id*="HEADER"]'
+    elements_to_remove = soup.select(selectors_to_remove)
+    for element in elements_to_remove:
+        element.extract()
+
+    # Render the parsed content as a string
+    rendered_content = soup.prettify()
+
+    # Return the parsed content as a response
+    return rendered_content
 
 if __name__ == "__main__":
     print(f"Starting server on {HOST}:{PORT}")
