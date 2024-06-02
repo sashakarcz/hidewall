a0c328f2df464301b6835b7628f7d64098223e85
Sasha Karcz <sasha@starnix.net>
2024-06-02 14:09:21 -0500

    enabing bucket backend for state

--- Terraform/envs/prod/backend.tf
+++ Terraform/envs/prod/backend.tf
@@ -1,13 +1,13 @@
-#terraform {
-#    backend "s3" {
-#        bucket        = "terraform-state"
-#        key           = "terraform.tfstate"
-#        region        = "us-east-1"
-#        endpoints     = { s3 = "https://us-ord-1.linodeobjects.com" }
-#        profile       = "linode-s3"
-#        skip_requesting_account_id = true
-#        skip_credentials_validation = true
-#        skip_metadata_api_check     = true
-#        use_path_style              = true
-#    }
-#}
+terraform {
+    backend "s3" {
+        bucket        = "terraform-state"
+        key           = "terraform.tfstate"
+        region        = "us-east-1"
+        endpoints     = { s3 = "https://us-ord-1.linodeobjects.com" }
+        profile       = "linode-s3"
+        skip_requesting_account_id = true
+        skip_credentials_validation = true
+        skip_metadata_api_check     = true
+        use_path_style              = true
+    }
+}
